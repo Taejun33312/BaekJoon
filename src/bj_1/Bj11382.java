18f373ccca4843911950e68324d21c4813f00152
Taejun33312 <a01055384863@gmail.com>
2024-03-04 15:44:27 +0900

    03-04 처음 부터 차근차근

---
+++ src/bj_1/Bj11382.java
@@ -0,0 +1,21 @@
+package bj_1;
+
+import java.util.Scanner;
+
+public class Bj11382 {
+	public static void main(String[] args) {
+		/**
+		 * 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+		 * 
+		 * 입력 첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 1012)이 공백을 사이에 두고 주어진다.
+		 * 
+		 * 출력 A+B+C의 값을 출력한다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		sc.close();
+		System.out.println(a+b+c);
+	}
+}
